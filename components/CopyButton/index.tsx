e61ac8f4c412e22cc3757cd553c98624d96c0459
ihorDudkevych <egorka01051993@gmail.com>
2023-12-07 01:29:33 +0200

    rework copy 2

--- components/CopyButton/index.tsx
+++ components/CopyButton/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { copyToClipboard } from '@/utils/helpers';
+import { copyToClipboard, fallbackCopyToClipboard } from '@/utils/helpers';
 import s from './index.module.scss';
 
 export const CopyButton = () => {
 	const handleClick = () => {
-		copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1');
+		copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1', fallbackCopyToClipboard);
 	};
 
 	return (
--- utils/helpers/copyToClipboard.ts
+++ utils/helpers/copyToClipboard.ts
@@ -1,16 +1,40 @@
 import { toastMessage } from '@/utils/helpers/toasttify';
 
-export const copyToClipboard = async (text: string) => {
+export const copyToClipboard = async (text: string, fallback: (text: string) => void): Promise<void> => {
 	try {
-		const permissionStatus = await navigator.permissions.query({ name: 'notifications' });
-		console.log('permissionStatus', permissionStatus);
-		if (permissionStatus.state === 'granted' || permissionStatus.state === 'prompt') {
+		if (document.queryCommandSupported('copy')) {
+			await toastMessage('queryCommandSupported - success', 'success');
+		}
+		if (navigator.clipboard) {
 			await window.navigator.clipboard.writeText(text);
 			await toastMessage('Скопировано', 'success');
+		} else {
+			fallback(text);
 		}
 	} catch (e) {
-		alert(e);
-		// await toastMessage('Не удалось копировать', 'warning');
-		await toastMessage(`${e}`, 'warning');
+		await toastMessage(
+			'Не удалось скопировать. Возможно, проблема в настройках безопасности на вашем устройстве. Скопируйте ссылку выше вручную.',
+			'warning'
+		);
 	}
+};
+
+// Fallback function for browsers that do not support Clipboard API
+export const fallbackCopyToClipboard = (text: string) => {
+	const textArea = document.createElement('textarea');
+	textArea.value = text;
+	document.body.appendChild(textArea);
+	textArea.select();
+
+	try {
+		document.execCommand('copy');
+		toastMessage('Скопировано', 'success');
+	} catch (e) {
+		toastMessage(
+			'Не удалось скопировать. Возможно, проблема в настройках безопасности на вашем устройстве. Скопируйте ссылку выше вручную.',
+			'warning'
+		);
+	}
+
+	document.body.removeChild(textArea);
 };
