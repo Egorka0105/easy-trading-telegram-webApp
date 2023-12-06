fe4964255b401bd9b33b82a18f6d2d0d72ac5544
ihorDudkevych <egorka01051993@gmail.com>
2023-12-06 12:03:23 +0200

    add form

---
+++ components/Loader/index.tsx
@@ -0,0 +1,18 @@
+import './index.scss';
+
+export const Loader = () => {
+  return (
+    <div className={'wavy'}>
+      <span className={'wave wave-1'}>L</span>
+      <span className={'wave wave-2'}>o</span>
+      <span className={'wave wave-3'}>a</span>
+      <span className={'wave wave-4'}>d</span>
+      <span className={'wave wave-5'}>i</span>
+      <span className={'wave wave-6'}>n</span>
+      <span className={'wave wave-7'}>g</span>
+      <span className={'wave wave-8'}>.</span>
+      <span className={'wave wave-9'}>.</span>
+      <span className={'wave wave-10'}>.</span>
+    </div>
+  );
+};
