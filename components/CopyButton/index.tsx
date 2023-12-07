a0977a16e602ec73baa973166bba43ed71abaaf0
ihorDudkevych <egorka01051993@gmail.com>
2023-12-07 16:06:01 +0200

    copy test 1

--- components/CopyButton/index.tsx
+++ components/CopyButton/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { copyToClipboard } from '@/utils/helpers';
+import {copyToClipboard, fallbackCopyToClipboard} from '@/utils/helpers';
 import s from './index.module.scss';
 
 export const CopyButton = () => {
 	const handleClick = () => {
-		// copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1', fallbackCopyToClipboard);
-		copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1');
+		copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1', fallbackCopyToClipboard);
+		// copyToClipboard('TPCrSKPgRzyjxsmJMLRt27HKMcVkXmHyn1');
 	};
 
 	return (
--- utils/helpers/copyToClipboard.ts
+++ utils/helpers/copyToClipboard.ts
@@ -1,38 +1,37 @@
 import { toastMessage } from '@/utils/helpers/toasttify';
 
-export const copyToClipboard = async (text: string, fallback?: (text: string) => void): Promise<void> => {
+export const copyToClipboard = async (text: string, fallback: (text: string) => void): Promise<void> => {
+    try {
+        if (navigator.clipboard) {
+            await navigator.clipboard.writeText(text);
+            await toastMessage('Скопировано', 'success');
+        } else {
+            fallback(text);
+        }
+    } catch (e) {
+        await toastMessage(
+            'Не удалось скопировать. Возможно, проблема в настройках безопасности на вашем устройстве. Скопируйте ссылку выше вручную.',
+            'warning'
+        );
+    }
+};
+
+// fallback function for browsers that do not support Clipboard
+export const fallbackCopyToClipboard = (text: string) => {
+	const textArea = document.createElement('textarea');
+	textArea.value = text;
+	document.body.appendChild(textArea);
+	textArea.select();
+
 	try {
-		if (navigator.clipboard) {
-			await navigator.clipboard.writeText(text);
-			await toastMessage('Скопировано', 'success');
-		}
-		// } else {
-		// 	fallback(text);
-		// }
+		document.execCommand('copy');
+		toastMessage('Скопировано 2', 'success');
 	} catch (e) {
-		await toastMessage(
+		toastMessage(
 			'Не удалось скопировать. Возможно, проблема в настройках безопасности на вашем устройстве. Скопируйте ссылку выше вручную.',
 			'warning'
 		);
 	}
-};
 
-// fallback function for browsers that do not support Clipboard
-// export const fallbackCopyToClipboard = (text: string) => {
-// 	const textArea = document.createElement('textarea');
-// 	textArea.value = text;
-// 	document.body.appendChild(textArea);
-// 	textArea.select();
-//
-// 	try {
-// 		document.execCommand('copy');
-// 		toastMessage('Скопировано 2', 'success');
-// 	} catch (e) {
-// 		toastMessage(
-// 			'Не удалось скопировать. Возможно, проблема в настройках безопасности на вашем устройстве. Скопируйте ссылку выше вручную.',
-// 			'warning'
-// 		);
-// 	}
-//
-// 	document.body.removeChild(textArea);
-// };
+	document.body.removeChild(textArea);
+};
